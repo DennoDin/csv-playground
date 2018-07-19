be46eb2ba33406b930135a65bcd594f20b639421
Dustin <dustin.tran@live.com>
2018-07-19 11:57:25 +0900

    created working populatron

--- populatron.js
+++ populatron.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
+const parse = require("csv-parse");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    let total = 0;
+    fs.readFile("./cities.csv", (err, csvFile) => {
+      parse(csvFile, { columns: true }, (error, result) => {
+        result.forEach((city) => {
+          total += parseInt(city.population);
+        });
+        onFinished(total);
+      });
+    });
+  },
 };
